8bac16042798f5b58019314b73482e569b65608b
harutoreon <norabder567@gmail.com>
2023-09-19 04:19:10 +0900

    Add valid signup information

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -12,31 +12,37 @@ RSpec.describe "Users", type: :request do
     end
   end
 
-  context '有効な値の場合' do
-    let(:user_params) { { user: { name: 'Example User',
-                                  email: 'user@example.com',
-                                  password: 'password',
-                                  password_confirmation: 'password' } } }
-
-    it '登録されること' do
-      expect {
-        post users_path, params: user_params
-      }.to change(User, :count).by 1
-    end
- 
-    it 'users/showにリダイレクトされること' do
-      post users_path, params: user_params
-      expect(response).to redirect_to root_url  # サンプルアプリが最終形に達しているため、root_url とする。
-    end
-  end
-
   describe 'POST /users #create' do
-    it 'は、無効な値だと登録されないこと' do
-      expect { post users_path, params: { user: { name: '',
-                                                  email: 'user@invlid',
-                                                  password: 'foo',
-                                                  password_confirmation: 'bar' } }
-      }.to_not change(User, :count)
+    context '有効な値の場合' do
+      let(:user_params) { { user: { name: 'Example User',
+                                    email: 'user@example.com',
+                                    password: 'password',
+                                    password_confirmation: 'password' } } }
+  
+      it '登録されること' do
+        expect {
+          post users_path, params: user_params
+        }.to change(User, :count).by 1
+      end
+   
+      it 'トップページにリダイレクトされること' do
+        post users_path, params: user_params
+        expect(response).to redirect_to root_url  # サンプルアプリが最終形に達しているため、root_url とする。
+      end
+  
+      it 'flashが表示されること' do
+        post users_path, params: user_params
+        expect(flash).to be_any
+      end
+    end
+    context '無効な値の場合' do
+      it '登録されないこと' do
+        expect { post users_path, params: { user: { name: '',
+                                                    email: 'user@invlid',
+                                                    password: 'foo',
+                                                    password_confirmation: 'bar' } }
+        }.to_not change(User, :count)
+      end      
     end
   end
 end
