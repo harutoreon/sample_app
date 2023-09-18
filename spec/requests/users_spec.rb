9e743fc5497e587d66e632075abb19e2f04b3ca0
harutoreon <norabder567@gmail.com>
2023-09-19 03:28:30 +0900

    Add test against valid user registration

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -12,6 +12,24 @@ RSpec.describe "Users", type: :request do
     end
   end
 
+  context '有効な値の場合' do
+    let(:user_params) { { user: { name: 'Example User',
+                                  email: 'user@example.com',
+                                  password: 'password',
+                                  password_confirmation: 'password' } } }
+
+    it '登録されること' do
+      expect {
+        post users_path, params: user_params
+      }.to change(User, :count).by 1
+    end
+ 
+    it 'users/showにリダイレクトされること' do
+      post users_path, params: user_params
+      expect(response).to redirect_to root_url  # サンプルアプリが最終形に達しているため、root_url とする。
+    end
+  end
+
   describe 'POST /users #create' do
     it 'は、無効な値だと登録されないこと' do
       expect { post users_path, params: { user: { name: '',
