8e3c7bd3a21ab79c1645b9e1be9d6e1a6d04ba0d
harutoreon <norabder567@gmail.com>
2023-09-18 11:19:19 +0900

    Testing for invalid user registration

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -11,4 +11,14 @@ RSpec.describe "Users", type: :request do
       expect(response.body).to include full_title('Sign up')
     end
   end
+
+  describe 'POST /users #create' do
+    it 'は、無効な値だと登録されないこと' do
+      expect { post users_path, params: { user: { name: '',
+                                                  email: 'user@invlid',
+                                                  password: 'foo',
+                                                  password_confirmation: 'bar' } }
+      }.to_not change(User, :count)
+    end
+  end
 end
