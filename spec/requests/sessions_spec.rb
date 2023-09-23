03679f520b4232769ed2c4cba702675f747b372e
harutoreon <norabder567@gmail.com>
2023-09-23 19:18:12 +0900

    Add user logout test

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -9,13 +9,24 @@ RSpec.describe "Sessions", type: :request do
   end
 
   describe 'DELETE /logout' do
-    it 'ログアウトできること' do
+    before do
       user = FactoryBot.create(:user)
-      post login_path, params: { session: { email: user.email,
-                                            password: user.password } }
+      post login_path, params: { session: { email: user.email, password: user.password } }
+    end
+
+    it 'ログアウトできること' do
+      # user = FactoryBot.create(:user)
+      # post login_path, params: { session: { email: user.email,
+      #                                       password: user.password } }
       expect(logged_in?).to be_truthy
       delete logout_path
       expect(logged_in?).to_not be_truthy
+    end
+
+    it '2回連続でログアウトしてもエラーにならないこと' do
+      delete logout_path
+      delete logout_path
+      expect(response).to redirect_to root_path
     end
   end
 end
