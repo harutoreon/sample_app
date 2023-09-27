7eeb7ef7237a0127bea8949031644c83a283f206
harutoreon <norabder567@gmail.com>
2023-09-28 02:42:59 +0900

    Add micropost ordering test

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
  
 RSpec.describe Micropost, type: :model do
-  let(:user) { FactoryBot.create(:user) }
+  let!(:user) { FactoryBot.create(:user) }
   let(:micropost) { user.microposts.build(content: 'Lorem ipsum') }
 
   it 'は、有効であること' do
@@ -11,6 +11,12 @@ RSpec.describe Micropost, type: :model do
   it 'のuser_idがない場合は、無効であること' do
     micropost.user_id = nil
     expect(micropost).to_not be_valid
+  end
+
+  it '並び順は投稿の新しい順になっていること' do
+    expect(FactoryBot.create(:most_recent)).to eq Micropost.first
+    expect(FactoryBot.create(:orange)).to eq Micropost.second
+    expect(FactoryBot.create(:cat_video)).to eq Micropost.last
   end
 
   describe 'content' do
